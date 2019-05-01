79f5d46ec1dbdd311d4e3f1c2ed0e25980b6ad95
Yunwen Wang <yunwen.wang.b@gmail.com>
2019-04-30 20:25:50 -0500

    instrument jaeger into redis-slave service

--- guestbook/php-redis/guestbook.php
+++ guestbook/php-redis/guestbook.php
@@ -8,17 +8,20 @@ require_once dirname(__FILE__).'/vendor/autoload.php';
 
 Predis\Autoloader::register();
 
-use Jaeger\Factory;
-// use GuzzleHttp\Client;
+use Jaeger\Config;
 use OpenTracing\Formats;
-$agent_port = '192.168.99.101';
-// init server span
-$factory = Factory::getInstance();
-$tracer = $factory->initTracer('user', $agent_port, 6831);
-$serverSpan = $tracer->startSpan('example HTTP', []);
-// $client = new Client;
-$clientSapn = $tracer->startSpan('get', ['child_of' => $serverSpan]);
-$tracer->inject($clientSapn->getContext(), Formats\BINARY, $trace_id);
+
+unset($_SERVER['argv']);
+//init server span start
+$config = Config::getInstance();
+$config->gen128bit();
+$tracer = $config->initTrace('frontend', 'my-jaeger-agent:6831');
+
+$spanContext = $tracer->extract(Formats\TEXT_MAP, $_SERVER);
+$serverSpan = $tracer->startSpan('frontend HTTP', ['child_of' => $spanContext]);
+$serverSpan->addBaggageItem("version", "1.8.9");
+$tracer->inject($serverSpan->getContext(), Formats\TEXT_MAP, $_SERVER);
+//init server span end
 
 
 if (isset($_GET['cmd']) === true) {
@@ -26,7 +29,7 @@ if (isset($_GET['cmd']) === true) {
   if (getenv('GET_HOSTS_FROM') == 'env') {
     $host = getenv('REDIS_MASTER_SERVICE_HOST');
   }
-  header('Content-Type: application/json');
+  // header('Content-Type: application/json');
   // header("my-trace-id".$trace_id);
 
   // when get the message
@@ -45,29 +48,37 @@ if (isset($_GET['cmd']) === true) {
       $host = getenv('REDIS_SLAVE_SERVICE_HOST');
     }
 
-    $client = new Predis\Client([
-      'scheme' => 'tcp',
-      'host'   => $host,
-      'port'   => 6379,
-      // 'parameters' => [
-      //   'headers': $trace_id,
-      // ],
-    ]);
+    $clientTrace = $config->initTrace('redis');
+    $injectTarget1 = [];
+    $clientSapn1 = $clientTrace->startSpan($host, ['child_of' => $spanContext]);
+    $clientTrace->inject($clientSapn1->spanContext, Formats\TEXT_MAP, $injectTarget1);
+
+    // echo sizeof($injectTarget1);
+
+    $arr = [];
+    $arr['scheme'] = 'tcp';
+    $arr['host'] = $host;
+    $arr['port'] = 6379;
+    foreach ($injectTarget1 as $k => $v) {
+      $arr[$k] = $v;
+    }
+    $url = $host."6379";
+
+    $client = new Predis\Client($arr);
+
+    $clientSapn1->setTags(['http.status_code' => 200, 'http.method' => 'GET', 'http.url' => $url]);
+    // $clientSapn1->log(['message' => "HTTP1 ". $method .' '. $url .' end !']);
+    $clientSapn1->finish();
+    //client span1 end
+
     $value = $client->get($_GET['key']);
-    // $url = 'http://127.0.0.1:8080';
-    // $result = $client->get($url, ['headers' => [ 'My-Trace-Id' => $trace_id ]]);
-
-    // $clientSapn->setTags(['http.url' => $url]);
-    $clientSapn->setTags(['http.method' => 'GET']);
-    // $clientSapn->setTags(['http.status' => $value->getStatusCode()]);
-
-    // $clientSapn->setTags(['http.result' => $result->getBody()]);
-    $clientSapn->finish();
-
-    $clientSapn = $tracer->startSpan('get', ['child_of' => $serverSpan]);
-
     print('{"data": "' . $value . '"}');
   }
 } else {
   phpinfo();
-} ?>
+} 
+//server span end
+$serverSpan->finish();
+//trace flush
+$config->flush();
+?>
